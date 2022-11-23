675be6b31995a0dc152bf79972bd63dc5158bf02
endalk200 <eb808826@gmail.com>
2022-11-23 11:42:08 +0300

    feat: added collections CRUD procedures and removed example procedure

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { collectionsRouter } from "./collections";
 
 export const appRouter = router({
-    example: exampleRouter,
+    collection: collectionsRouter,
     auth: authRouter,
 });
 
